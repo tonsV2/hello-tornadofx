26c799e2b8540f3a3c7848fba2e33aa0a2a3b70f
sj <sebastianthegreatful@gmail.com>
2018-10-16 17:12:07 +0200

    Initial commit

---
+++ src/main/kotlin/dk/fitfit/tornadofx/Main.kt
@@ -0,0 +1,31 @@
+package dk.fitfit.tornadofx
+
+import javafx.scene.text.FontWeight
+import tornadofx.*
+
+class HelloWorld : View() {
+    override val root = hbox {
+        label("Hello world")
+    }
+}
+
+class HelloWorldApp : App(HelloWorld::class, Styles::class)
+
+class Styles : Stylesheet() {
+    init {
+        label {
+            fontSize = 40.px
+            fontWeight = FontWeight.BOLD
+            backgroundColor += c("#cecece")
+        }
+    }
+}
+
+open class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch<HelloWorldApp>(args)
+        }
+    }
+}
